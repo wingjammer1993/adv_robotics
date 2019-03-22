e87351a164529b39f82b339586014097016b7d9c
wingjammer1993 <hail.radical@gmail.com>
2019-03-21 19:42:04 -0600

    helper for git

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wingjammer/Documents/vcs/adv_robotics/catkin_ws/src
-# Build directory: /home/wingjammer/Documents/vcs/adv_robotics/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(bldc_control)
-subdirs(my_pkg)
