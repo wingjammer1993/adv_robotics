741a06c54b283c031720cdab4cbebd8e5feabd08
Vachan D A <vaas4627@colorado.edu>
2019-04-22 14:26:17 -0600

    Adds stop sign package.

---
+++ catkin_ws/src/stop_sign/src/detector.py
@@ -0,0 +1,78 @@
+from skimage.transform import pyramid_gaussian
+import argparse
+import cv2
+import time
+import numpy as np
+import glob
+from matplotlib import pyplot as plt
+
+class Detector(object):
+    def __init__(self, target, image_size, debug=False, win_size=16, threshold = 8e-5, step_size=2):
+        self.win_size = win_size
+        self.target = cv2.resize(target, (win_size, win_size))
+        self.threshold = threshold
+        self.step_size = step_size
+        self.image_size = image_size
+        self.debug = debug
+        
+    def sliding_window(self, image, step_size, window_size):
+        for y in range(0, image.shape[0], step_size):
+            for x in range(0, image.shape[1], step_size):
+                yield (x, y, image[y:y+window_size[1], x:x+window_size[1]])
+
+    def meanSquareError(self, target, image):
+        assert image.shape == target.shape, "Images must be the same shape."
+        error = np.sum((image.astype("float") - target.astype("float")) ** 2)
+        error = error/float(image.shape[0] * image.shape[1] * image.shape[2])
+        return error
+
+    def get_ssim(self, image):
+        start = time.time()
+
+        max_ssim = -1
+        max_img = None
+        max_bbox = []
+
+        while image.shape[0] > 16 and  image.shape[1] > 16:
+            image = cv2.pyrDown(image)
+            for (x, y, window) in self.sliding_window(
+                    image, step_size=self.step_size,
+                    window_size=(self.win_size, self.win_size)
+                ):
+                if window.shape[0] != 16 or window.shape[1] != 16:
+                    continue
+
+                ssim = 1./self.meanSquareError(self.target, image[y:y+16, x:x+16])
+                if ssim > max_ssim:
+                    max_ssim = ssim
+                    max_img = image.copy()
+                    max_bbox = [(x, y), (x + 16, y + 16)]
+
+                if self.debug:
+                    clone = image.copy()
+                    cv2.rectangle(clone, (x, y), (x + 16, y + 16), (255, 0, 0), 2)
+                    cv2.imshow("Window",clone)
+                    cv2.waitKey(1)
+                    time.sleep(0.025)
+
+        if self.debug:
+            if max_ssim < self.threshold:
+                cv2.rectangle(max_img, max_bbox[0], max_bbox[1], (0, 0, 255), 2)
+            else:
+                cv2.rectangle(max_img, max_bbox[0], max_bbox[1], (0, 255, 0), 2)
+
+            cv2.imwrite("{}.png".format(name), max_img)
+            cv2.destroyAllWindows()
+
+        end = time.time()
+        return max_ssim
+
+    def contains_ss(self, image, debug=False):
+        if image.shape != self.image_size:
+            image = cv2.resize(image, self.image_size)
+        max_ssim = self.get_ssim(image)
+        return max_ssim > self.threshold
+
+    @staticmethod
+    def compareImages(a, b):
+        return 1./meanSquareError(a, b)
---
+++ catkin_ws/src/stop_sign/src/stop_sign.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import rospy
+import rospkg
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import numpy as np
+from time import time
+
+from detector import Detector
+
+from sensor_msgs.msg import Image
+from std_msgs.msg import Bool
+
+
+from threading import Event
+
+class StopSign(object):
+    def __init__(self, detector):
+        self.event = Event()
+        self.event.set()
+        self.bridge = CvBridge()
+
+        self.detector = detector
+
+        # Publishes object recognition prediction
+        self.stop_sign_pub = rospy.Publisher(
+            'stop_sign/out',
+             Bool,
+             queue_size=1
+         )
+
+        # Subscribes to rs_camera color
+        self.image_sub = rospy.Subscriber(
+            '/camera/color/image_raw',
+            Image,
+            self.image_callback,
+            queue_size=1,
+            buff_size=320000000
+        )
+
+    def image_callback(self, msg):
+        # Perform prediction per the set frequency
+        if self.event.isSet():
+    	    self.event.clear()
+            # Convert ros image to cv image
+            try:
+                image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            except CvBridgeError, e:
+                return
+
+            contains = self.detector.contains_ss(image)
+            if contains:
+                print "Stop sign detected."
+                self.stop_sign_pub.publish(True)
+            else:
+                print "No stop sign detected."
+                self.stop_sign_pub.publish(False)
+            self.event.set()
+
+if __name__ == '__main__':
+    rospy.init_node('stop_sign', anonymous=True)
+    r = None
+    try:
+        r = rospkg.RosPack()
+        target = r.get_path("stop_sign")+"/data/stop_sign.png"
+        print(target)
+    except IOError, e:
+        print e
+
+    detector = Detector(cv2.imread(target), (320, 240), debug=True)
+    StopSign(detector)
+    rospy.spin()
